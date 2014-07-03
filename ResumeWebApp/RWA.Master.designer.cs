3550b6ad1b5e65616b54f58f6c1ef37d345a93cc
gamers542 <gamers542@yahoo.com>
2014-07-02 22:13:11 -0500

    added a sticky footer for all the webpages

--- ResumeWebApp/RWA.Master.designer.cs
+++ ResumeWebApp/RWA.Master.designer.cs
@@ -20,5 +20,14 @@ namespace ResumeWebApp {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.ContentPlaceHolder head;
+        
+        /// <summary>
+        /// ContentPlaceHolder1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder ContentPlaceHolder1;
     }
 }
